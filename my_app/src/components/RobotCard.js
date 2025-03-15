ba7e256be573ee1f2057516358bba35143d7dafd
alvaro <a.baccap@uniandes.edu.co>
2025-03-15 14:14:29 -0500

    [WIP] Robot image connected to backend

--- my_app/src/components/RobotCard.js
+++ my_app/src/components/RobotCard.js
@@ -1,5 +1,5 @@
 import * as PropTypes from "prop-types";
-import {Card} from "react-bootstrap";
+import {Card, CardImg} from "react-bootstrap";
 import {useEffect, useState} from "react";
 
 const URL = "http://localhost:3001/robots";
@@ -7,15 +7,31 @@ const URL = "http://localhost:3001/robots";
 
 export default function RobotCard(props) {
 
-    console.log("Robot actualizado");
+    const [robotImage, setRobotImage] = useState(null);
+
+    function getRobotDetails(url, robotId) {
+        return fetch(`${url}/${robotId}`, {})
+            .then(response => response.json())
+    }
+
+    useEffect(() => {
+        getRobotDetails(URL, props.robot.id)
+            .then((robotDetails) => {
+                setRobotImage(robotDetails.imagen
+                    .replace("github.com", "raw.githubusercontent.com")
+                    .replace("blob/", "")
+                );
+            });
+    }, [props.robot.id]);
 
     return (
         <Card>
             <Card.Title>{props.robot.nombre}</Card.Title>
+            <Card.Img src={robotImage} alt={props.robot.id} className="rounded" />
             <Card.Body>
-                <Card.Text>Año de Fabricación: ${props.robot.añoFabricacion}</Card.Text>
-                <Card.Text>Capacidad de Procesamiento: ${props.robot.capacidadProcesamiento}</Card.Text>
-                <Card.Text>Humor: ${props.robot.humor}</Card.Text>
+                <Card.Text>Año de Fabricación: {props.robot.añoFabricacion}</Card.Text>
+                <Card.Text>Capacidad de Procesamiento: {props.robot.capacidadProcesamiento}</Card.Text>
+                <Card.Text>Humor: {props.robot.humor}</Card.Text>
             </Card.Body>
         </Card>
     )
